90cd10806f2af760520f6a22fe742b54dc6c21f1
robberbadeia <robber.badeia.com>
2023-10-05 12:50:26 +0300

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
 
-    ln = len(sys.argv) - 1
+    ln = len(argv) - 1
     if (ln == 0):
         print("{} argument.".format(ln))
     elif (ln == 1):
         print("{} argument:".format(ln))
     else:
         print("{} arguments:".format(ln))
-    for i in range(ln):
-        print("{:d}:{}".format((sys.argv + 1), sys.args[i + 1]))
+    for i in range(1, ln):
+        print("{}: {}".format(i, argv))
