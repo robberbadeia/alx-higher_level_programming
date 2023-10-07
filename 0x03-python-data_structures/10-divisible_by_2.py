817a55a5ff64ccb3fa22363ffce52676bfd2448f
robberbadeia <robber.badeia.com>
2023-10-07 18:54:03 +0300

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):    
-    if my_list:
-        numbers = []
-        for i in my_list:
-            if i % 2 is 0:
-                numbers.append(True)
-            else:
-                numbers.append(False)
-        return numbers
+    numbers = []
+    for i in my_list:
+        if (i % 2 == 0):
+            numbers.append(True)
+        else:
+            numbers.append(False)
+    return numbers
