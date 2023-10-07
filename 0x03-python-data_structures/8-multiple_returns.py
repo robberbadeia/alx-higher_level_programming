726c54bdafabbb9aa67be2b2bbdb41698e314f74
robberbadeia <robber.badeia.com>
2023-10-07 18:25:25 +0300

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if (len(sentence) > 0):
-        ln = len(sentence) 
+        ln = len(sentence)
         first_char = sentence[0]
         return (ln, first_char)
     else:
