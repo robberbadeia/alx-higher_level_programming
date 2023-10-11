9ac4f1bde76d5e54c759d78417c7cf6704f5bfd1
robberbadeia <robber.badeia.com>
2023-10-11 12:59:10 +0300

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if (matrix):
-        new_matrix = list(map(lambda x:
+    new_matrix = list(map(lambda x:
                               list(map(lambda y: (y ** 2), x)), matrix))
     return(new_matrix)
