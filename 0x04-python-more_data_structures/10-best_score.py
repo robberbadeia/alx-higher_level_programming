77a2c9429eb45a120b9ebd14c79026c3daa3b663
robberbadeia <robber.badeia.com>
2023-10-11 20:33:26 +0300

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        return None
-    _max = max(a_dictionary, key=a_dictionary.get)
-    return(_max)
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
