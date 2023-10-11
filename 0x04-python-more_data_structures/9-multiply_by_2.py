22784779736bfe155adc2cfef9d7e2192cc1d1ed
robberbadeia <robber.badeia.com>
2023-10-11 16:18:30 +0300

    Task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    a_dictionary.update((x, y * 2) for x, y in a_dictionary.items())
-    return (a_dictionary)
+    _cp_dec = a_dictionary.copy()
+    _cp_dec.update((x, y * 2) for x, y in a_dictionary.items())
+    return (_cp_dec)
