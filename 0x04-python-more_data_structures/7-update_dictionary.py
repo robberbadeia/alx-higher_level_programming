91c01391d8741643d126d0a0df1a3176e8afad90
robberbadeia <robber.badeia.com>
2023-10-11 15:01:30 +0300

    Task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-	a_dictionary['key'] = value
-	return (a_dictionary)
+    _dic = {key: value}
+    a_dictionary.update(_dic)
+    return (a_dictionary)
