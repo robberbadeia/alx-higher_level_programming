5284b82e0a0f1de572fe4271d03a16bb500aa68e
robberbadeia <robber.badeia@gmail.com>
2023-10-29 15:03:19 +0200

    Task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Rectangle Class
+
+    Raises:
+        TypeError: if width not int
+        ValueError: if width < 0
+        TypeError: if height not int
+        ValueError: if height < 0
+"""
+
+
+class Rectangle:
+    """Class Defination
+    """
+    def __init__(self, width=0, height=0):
+        """Rectangle Intialization
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Return width value"""
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Set widht value"""
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Return height value"""
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Set height value"""
+        if (type(value) != int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate rectangler area"""
+        return(self.__height * self.__width)
+
+    def perimeter(self):
+        """Claculate Rectangler preimeter"""
+        if (self.__height == 0 or self.__width == 0):
+            return 0
+        return (2 * (self.__height + self.__width))
