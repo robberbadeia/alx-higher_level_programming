9a56adc28b63a1f6f572ffc2f4464a939b7a0b56
robberbadeia <robber.badeia@gmail.com>
2023-10-29 14:33:00 +0200

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,9 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Rectangle Intialization
         """
-        self.width = width
         self.height = height
+        self.width = width
+
 
     @property
     def width(self):
