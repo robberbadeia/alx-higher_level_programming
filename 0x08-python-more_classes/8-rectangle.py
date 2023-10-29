4d3ab7bb047167a2abd18d5e689ec6d76c05d771
robberbadeia <robber.badeia@gmail.com>
2023-10-29 20:20:51 +0200

    Task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -86,6 +86,7 @@ class Rectangle:
         type(self).number_of_instances += 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Check who is bigger"""
         if (type(rect_1) != Rectangle):
