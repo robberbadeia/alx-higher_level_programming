fbb1faee4d95246d233d24cb5cdb108384ce816c
robberbadeia <robber.badeia@gmail.com>
2023-10-30 15:06:07 +0200

    up T6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -78,5 +78,5 @@ class Rectangle:
 
     def __del__(self):
         """Delete instance"""
-        print("Bye rectangle...")
         type(self).number_of_instances -= 1
+        print("Bye rectangle...")
