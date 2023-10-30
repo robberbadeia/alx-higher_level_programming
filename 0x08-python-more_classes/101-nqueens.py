f5f08a2fd236ea944e09149b4605aec050abade8
robberbadeia <robber.badeia@gmail.com>
2023-10-30 20:13:50 +0200

    T101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
+import sys
+def argChecker(args):
+    """ Check the ARGS"""
+    if len(args) < 2 or len(args) > 2:
+        print ("Usage: nqueens N")
+        exit(1)
+
+    if not args[1].isdigit():
+        print("N must be a number")
+        exit(1)
+
+    if (int(args[1]) < 4):
+        print("N must be at least 4")
+        exit(1)
 
 
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    argChecker(args)
