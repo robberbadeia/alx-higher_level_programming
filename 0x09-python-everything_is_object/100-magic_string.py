8baa088daeec5c17f4b5b985b6f15df61e2c7fbc
robberbadeia <robber.badeia@gmail.com>
2023-10-31 15:02:01 +0200

    Prj 9

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """_summary_
 """
-def magic_string():
-    """_summary_
 
-    Returns:
-        _type_: _description_
-    """
+
+def magic_string():
     return ("BestSchool")
