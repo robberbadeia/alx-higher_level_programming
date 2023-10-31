040a58b2c32848ab9a0bd24e9713d1935b28bc3c
robberbadeia <robber.badeia@gmail.com>
2023-10-31 15:00:02 +0200

    Prj 9

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""_summary_
+"""
 def magic_string():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     return ("BestSchool")
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+class LockedClass():
+    """Represintation
+    """
+    __slots__ = "first_name"
