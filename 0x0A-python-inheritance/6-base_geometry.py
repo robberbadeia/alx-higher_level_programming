dab7774d9c751c4f2995c711a61030a0c34a5a20
robberbadeia <robber.badeia@gmail.com>
2023-11-05 17:15:20 +0200

    T06

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Class Implementation"""
+
+    def area(self):
+        """Function Implementation"""
+
+        raise Exception("area() is not implemented")
