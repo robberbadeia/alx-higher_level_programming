3a34d4e60d9af00578eeb4ea29b3535aba354e5a
robberbadeia <robber.badeia@gmail.com>
2023-11-05 15:14:43 +0200

    T00

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Function
+"""Function to return all attributes and methodes of an object
 """
 
 
 def lookup(obj):
     """returns the list of available attributes and methods of an object"""
 
-    return (dir(object))
+    return (dir(obj))
