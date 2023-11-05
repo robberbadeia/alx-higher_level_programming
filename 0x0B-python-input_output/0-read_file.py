bf8595b0b2aca0000d3cbc902d472b713a5ef579
robberbadeia <robber.badeia@gmail.com>
2023-11-05 20:12:36 +0200

    T00

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function to read from a file and print text"""
+
+
+def read_file(filename=""):
+    """Function Implementation"""
+
+    with open(filename, 'r', encoding="utf-8") as file:
+        text = file.read()
+        print(text)
