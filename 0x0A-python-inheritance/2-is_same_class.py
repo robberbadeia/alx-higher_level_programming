5c12e7a9258c29710d45f21fe8b81043308eefc9
robberbadeia <robber.badeia@gmail.com>
2023-11-05 16:33:03 +0200

    T02

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Function return True if an object of the class otherwise false"""
+
+
+def is_same_class(obj, a_class):
+    """Function implementation"""
+
+    if (type(obj) == a_class):
+        return True
+    else:
+        return False
