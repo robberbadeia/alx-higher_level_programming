24e1419632eec5625955aa95a95b17083c33ca1b
robberbadeia <robber.badeia@gmail.com>
2023-11-06 21:09:07 +0200

    T05

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""writes an Object to a text file, using a JSON representation"""
+
+
+def save_to_json_file(my_obj, filename):
+    """Function Implementation"""
+
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
