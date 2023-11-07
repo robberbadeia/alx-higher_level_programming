45795ec366ec12c413d0e9320427452ac5c5c108
robberbadeia <robber.badeia@gmail.com>
2023-11-07 12:55:14 +0200

    T02

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,5 @@
 
 def append_write(filename="", text=""):
     """Function Implementation"""
-
     with open(filename, 'a', encoding="utf-8") as file:
-        nb = file.write(text)
-        return(nb)
+        return file.write(text)
