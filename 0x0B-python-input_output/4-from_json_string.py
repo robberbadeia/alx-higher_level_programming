f059c8e572f619db853885c3cfe5b6f4a11481b5
robberbadeia <robber.badeia@gmail.com>
2023-11-07 13:00:29 +0200

    T04

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import json
 """returns an object (Python data structure) represented by a JSON string"""
+import json
 
 
 def from_json_string(my_str):
     """Function Implementation"""
-
     return json.loads(my_str)
