99858376081b085a115f35a2110222e36028f27c
robberbadeia <robber.badeia@gmail.com>
2023-11-07 13:05:25 +0200

    T06

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import json
 """creates an Object from a "JSON file"""
+import json
 
 
 def load_from_json_file(filename):
     """Function Implementation"""
-
     with open(filename) as file:
         return json.load(file)
