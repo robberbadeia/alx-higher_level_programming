76919d790d196d1793303a089e8a9e96ecf4c950
robberbadeia <robber.badeia@gmail.com>
2023-11-07 13:30:06 +0200

    T08

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 
+returns the dictionary description with simple data structure
+for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """Function Implementation"""
+    return obj.__dict__
