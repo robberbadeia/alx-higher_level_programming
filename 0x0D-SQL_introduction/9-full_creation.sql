8f45705cd834d12445d80b516fa64eff037c7ab3
RobberBadeia <robber.badeia@gmail.com>
2023-12-12 18:17:01 +0200

    T09

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
         score INT
     );
 INSERT INTO second_table VALUES
-    (1, "John", 10)و
-    (2, "Alex", 3)و
-    (3, "Bob", 14)و
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
     (4, "George", 8)
