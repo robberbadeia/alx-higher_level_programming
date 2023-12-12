175ed0ed29036dd40ceaf65d03af170e69fec869
RobberBadeia <robber.badeia@gmail.com>
2023-12-12 15:08:27 +0200

    T05

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,4 @@
 -- T05. Full description
 -- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
-DESCRIBE first_table FROM hbtn_0c_0
+USE hbtn_0c_0
+DESCRIBE first_table
