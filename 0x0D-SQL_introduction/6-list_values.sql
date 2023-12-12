e616ada0b929053fcf69e859b2e4f1ad8d9fba02
RobberBadeia <robber.badeia@gmail.com>
2023-12-12 15:20:12 +0200

    T06

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,4 @@
 -- T06. List all in table
 -- Script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
-SELECT *
+USE hbtn_0c_0
+SELECT * FROM first_table
