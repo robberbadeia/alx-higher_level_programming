893f2a83cd8a8476a5d959341da8f4acebd45404
RobberBadeia <robber.badeia@gmail.com>
2023-12-12 15:41:08 +0200

    T08

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,8 @@
 -- T08. Count 89
 -- Script that displays the number of records with id = 89 in the table first_table
-COUNT(*) FROM first_table WHERE id=89
+SELECT 
+    COUNT(*) 
+FROM 
+    first_table
+WHERE 
+    id=89
