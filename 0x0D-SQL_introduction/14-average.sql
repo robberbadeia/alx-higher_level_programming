d8e12d85e06ccc8f38c3d93eeeb3c4bb0ee0cce3
RobberBadeia <robber.badeia@gmail.com>
2023-12-12 18:55:39 +0200

    T14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,6 @@
 -- T14. Average
 -- Script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-
+SELECT
+    AVG(score) 'average'
+FROM
+    second_table
