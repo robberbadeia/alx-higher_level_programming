89866caa127a42547afe167864f95660e6f60304
RobberBadeia <robber.badeia@gmail.com>
2023-12-12 18:04:36 +0200

    T09

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -14,4 +14,3 @@ INSERT INTO second_table VALUES (1, "John", 10)
 INSERT INTO second_table VALUES (2, "Alex", 3)
 INSERT INTO second_table VALUES (3, "Bob", 14)
 INSERT INTO second_table VALUES (4, "George", 8)
-
