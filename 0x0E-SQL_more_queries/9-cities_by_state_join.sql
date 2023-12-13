44b111e49e3de4e6e64095c819e342caf9fb19b3
RobberBadeia <robber.badeia@gmail.com>
2023-12-13 19:42:18 +0200

    T09

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,12 +1,14 @@
 -- T09. Cities by States
 -- Script that lists all cities contained in the database hbtn_0d_usa.
 SELECT 
-    id,
+    cities.id,
     cities.name,
     states.name
 FROM
     states 
     FULL OUTER JOIN
     cities
-ON
-    states.id = cities.state_id;
+WHERE
+    states.id = cities.state_id
+ORDER BY
+    cities.id ASC
