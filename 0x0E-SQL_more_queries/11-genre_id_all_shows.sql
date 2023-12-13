409fe2869c99e04191946cec140bda72cde7533d
RobberBadeia <robber.badeia@gmail.com>
2023-12-13 21:40:33 +0200

    T11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,12 @@
+-- T11. Genre ID for all shows
+-- Script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT
+    tv_shows.title, tv_show_genres.genre_id
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres
+ON
+    tv_shows.id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title, tv_show_genres.genre_id ASC
