5165e40a7118a9ed4b14d4dde9c17706cca8cfe0
RobberBadeia <robber.badeia@gmail.com>
2023-12-13 13:21:29 +0200

    T00

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- T00. My privileges!
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
+
