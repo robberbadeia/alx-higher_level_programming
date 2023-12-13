c1ff54c7eac49c0274e0cd67333c8dada59bf4cc
RobberBadeia <robber.badeia@gmail.com>
2023-12-13 14:04:30 +0200

    T01

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- T01. Root user
+-- Script that creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
