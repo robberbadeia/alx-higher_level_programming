e6c6c69aa02a27295faf10f83ebd0a176fad57b1
RobberBadeia <robber.badeia@gmail.com>
2024-01-08 13:11:40 +0200

    JS01

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// print "C is fun"
+// print "Python is cool"
+// print "JavaScript is amazing"
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
