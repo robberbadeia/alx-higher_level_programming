ba85cab99382ce0a9b6ed8800de396f138ebaa5a
RobberBadeia <robber.badeia@gmail.com>
2024-01-08 16:06:39 +0200

    JS11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,11 @@
 // script that searches the second biggest integer in the list of arguments.
 
 function secondBigInt (lst) {
-  return (lst.sort().reverse()[1]);
+  if (lst.lenght <= 3)
+  {
+    return (0);
+  } else {
+    return (lst.sort().reverse()[1]);
+  }
 }
 console.log(secondBigInt(process.argv.slice(2)));
