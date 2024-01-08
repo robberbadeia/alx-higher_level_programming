719dc49fc18351728b1a38b41c26094005052af2
RobberBadeia <robber.badeia@gmail.com>
2024-01-08 14:34:27 +0200

    JS05

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer
 if (isNaN(process.argv[2])) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number: ' + ~~process.argv[2]);
+  console.log('My number: ' + ~~process.argv[2]);
 }
