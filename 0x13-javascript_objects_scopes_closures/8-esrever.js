52422df4b3f3eff4dd0124de52cbb84f73ac288c
RobberBadeia <robber.badeia@gmail.com>
2024-01-09 18:32:04 +0200

    JS08

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 // Write a function that returns the reversed version of a list
 exports.esrever = function (list) {
+  const newLst = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    newLst.push(list[i]);
+  }
+  return (newLst);
 };
