b1c40e891726e7d49081603256337c4388277a7e
RobberBadeia <robber.badeia@gmail.com>
2024-01-09 16:21:48 +0200

    JS07

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Write a function that returns the number of occurrences in a list
-exports.nbOccurences = function (list, searchElement) {   
-}
+exports.nbOccurences = function (list, searchElement) {
+};
