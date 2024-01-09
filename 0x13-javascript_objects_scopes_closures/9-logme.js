490ad65f61737776ad17bab5d62a3ffff6686b78
RobberBadeia <robber.badeia@gmail.com>
2024-01-09 18:42:43 +0200

    JS09

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 // Write a function that prints the number of arguments already printed and the new argument value
 exports.logMe = function (item) {
+    const lst = [];
+    lst.push(item);
+    console.log(lst);
 };
