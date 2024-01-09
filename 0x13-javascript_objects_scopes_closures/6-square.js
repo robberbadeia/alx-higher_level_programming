b4b203315721bcb9180127514a9ea94da82c254f
RobberBadeia <robber.badeia@gmail.com>
2024-01-09 13:57:27 +0200

    JS06

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -13,7 +13,7 @@ module.exports = class Square extends Rectangle {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }
