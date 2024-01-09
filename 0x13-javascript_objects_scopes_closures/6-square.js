36f86fd3f142ba050bb529f6b4ef27cd6eeed4e1
RobberBadeia <robber.badeia@gmail.com>
2024-01-09 13:55:54 +0200

    JS06

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+// Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('C'.repeat(this.width));
+      }
+    }
+  }
+};
