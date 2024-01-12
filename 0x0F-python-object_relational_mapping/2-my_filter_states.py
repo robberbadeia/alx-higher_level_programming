4be1fa05f099ac6becd41ab97eb0585256401164
RobberBadeia <robber.badeia@gmail.com>
2024-01-12 09:28:14 +0200

    T03

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,13 @@ if __name__ == "__main__":
                          db=argv[3])
 
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".format(argv[4])
+    query = '''
+            SELECT *
+            FROM states
+            WHERE name
+            LIKE '{:s}'
+            ORDER BY id ASC
+            '''.format(argv[4])
     cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
