0a6083d01a19a5c73231b8ef4949cfddac830500
RobberBadeia <robber.badeia@gmail.com>
2024-01-12 09:25:52 +0200

    T03

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
-                         db=argv[3],
-                         state=argv[4])
+                         db=argv[3])
 
     cursor = db.cursor()
-    query = "SELECT * FROM states ORDER BY id ASC"
+    query = "SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".format(argv[4])
     cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cursor.close()
     db.close()
