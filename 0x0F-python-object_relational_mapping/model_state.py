a86210933ff6ac96a10170a7ca66ed187d4a14c0
RobberBadeia <robber.badeia@gmail.com>
2024-01-12 16:57:02 +0200

    T06

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,6 +13,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    class Implementation
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
