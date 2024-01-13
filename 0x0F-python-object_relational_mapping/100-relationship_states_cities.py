6698f21dd1c6a088c688705bd796f56e3ef2e06d
RobberBadeia <robber.badeia@gmail.com>
2024-01-13 16:28:15 +0200

    T15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """
 script that creates the State “California”
 with the City “San Francisco” from the database hbtn_0e_100_usa
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Write a python file that
 contains the class definition of a State
