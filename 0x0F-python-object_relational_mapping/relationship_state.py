37541bbfd06ab87e0beb3024bac8bb405d3eae3b
RobberBadeia <robber.badeia@gmail.com>
2024-01-13 17:05:34 +0200

    T100

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -20,4 +20,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state", cascade="all, delete")
+    cities = relationship("City", back_populates="state", backref='state',cascade="all, delete")
