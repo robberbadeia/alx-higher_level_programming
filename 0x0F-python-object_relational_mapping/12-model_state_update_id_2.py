7d708dc53fec62a6f055e50e2d318b2f7f750b95
RobberBadeia <robber.badeia@gmail.com>
2024-01-13 13:48:11 +0200

    T12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     session.query(State).\
-        filter(State.id.__eq__('2')).\
-            update(State.name.__eq__('New Mexico'))
+        filter(State.id.__eq__(2)).\
+            .first().\
+                pdate(State.name.__eq__('New Mexico'))
     session.commit()
     session.close()
