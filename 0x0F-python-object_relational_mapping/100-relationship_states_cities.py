1622d3c30fa53c95532de3e41e34eea602a61787
RobberBadeia <robber.badeia@gmail.com>
2024-01-13 17:08:28 +0200

    T100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
 """
-create state "California" with city attribute "San Francisco"
-parameters given to script: username, password, database
+script that creates the State “California”
+with the City “San Francisco” from the database hbtn_0e_100_usa
 """
-
-from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import create_engine
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
-
-    # make engine for database
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(user, passwd, db), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    """
+    main function
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # create state "California" with city attribute "San Francisco"
-    new_s = State(name="California")
-    new_c = City(name="San Francisco")
-    new_s.cities.append(new_c)
-
-    session.add(new_s)
-    session.add(new_c)
-
+    _state = State(id=1, name="California")
+    _city = City(id=1, name="San Francisco")
+    _state.cities.append(_city)
+    session.add(_state)
+    session.add(_city)
     session.commit()
     session.close()
