47e268d63679e3a985809f59cfa97090e82d9ca3
RobberBadeia <robber.badeia@gmail.com>
2024-01-13 17:00:42 +0200

    T100

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -18,6 +18,6 @@ class City(Base):
     class Implementation
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    state_id = Column(Integer, ForeignKey(State.id))
