4f8c96a2cfe0c35d74541229e99ea8c079a6e76c
RobberBadeia <robber.badeia@gmail.com>
2024-01-13 13:45:17 +0200

    T12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that changes the name of a State object
+from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    main function
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).\
+        filter(State.id.__eq__(2)).update(State.name.__eq__('New Mexico'))
+    session.commit()
+    session.close()
