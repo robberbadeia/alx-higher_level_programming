402847834e4b8d881552bdcb78bf2e81a4c881fa
RobberBadeia <robber.badeia@gmail.com>
2024-01-13 16:43:43 +0200

    T15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     _state = State(id=1, name="California")
     _city = City(id=1, name="San Francisco")
-    #_state.cities.append(_city)
+    _state.cities.append(_city)
 
     session.add(_state)
     session.add(_city)
