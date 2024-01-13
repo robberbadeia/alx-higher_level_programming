bfd800da37c7b925eea8c79f06aa69f054f4ca1a
RobberBadeia <robber.badeia@gmail.com>
2024-01-13 23:01:43 +0200

    T15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     """
     main function
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1],
                                    sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
