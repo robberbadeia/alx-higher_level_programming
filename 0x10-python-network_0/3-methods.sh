35a1b8ed52a9cae531496a8d9578a23307604a53
RobberBadeia <robber.badeia@gmail.com>
2024-01-25 15:23:58 +0200

    .

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS "$1"
+curl -sI 0.0.0.0:5000/route_4 | grep -i 'Allow' | cut -d ' ' -f2-
