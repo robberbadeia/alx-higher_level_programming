704fdce2902b1c971fafd556d18506e47e47b54a
RobberBadeia <robber.badeia@gmail.com>
2024-01-25 18:24:34 +0200

    .

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,9 +3,10 @@
 function that finds a peak in a list of unsorted integers
 """
 def find_peak(list_of_integers):
-    list_of_integers.sort()
-    if len(list_of_integers) != 0:
-        return(list_of_integers[-1])
-    return (None)
-
-
+    if len(list_of_integers) == 0:
+        return (None)
+    lenght = len(list_of_integers) - 1
+    while (lenght >= 0):
+        if (list_of_integers[lenght - 1] >= list_of_integers[lenght]):
+            return list_of_integers[lenght - 1]
+        lenght -= 1
