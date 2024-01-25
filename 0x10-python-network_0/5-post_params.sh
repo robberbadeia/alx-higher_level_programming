0e72754a0af1ede58d5563a5fdc04d035bf83c34
RobberBadeia <robber.badeia@gmail.com>
2024-01-25 16:40:54 +0200

    .

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take in URL, POST key:vals; Usage: ./5-post_params.sh 0.0.0.0:5000/route_6 ; echo ""
-curl -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST -s "$1"
