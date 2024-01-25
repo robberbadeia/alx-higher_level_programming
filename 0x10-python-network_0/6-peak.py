bd87022406604449d139891b4253e9466a6a03b2
RobberBadeia <robber.badeia@gmail.com>
2024-01-25 18:04:49 +0200

    .

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function that finds a peak in a list of unsorted integers
+"""
+def find_peak(list_of_integers):
+    list_of_integers.sort()
+    if len(list_of_integers) != 0:
+        return(list_of_integers[-1])
+    return (None)
+
+
