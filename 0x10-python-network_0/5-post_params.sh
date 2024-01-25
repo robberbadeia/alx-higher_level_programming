d02e7a230b3d0c77e7d21d9fe644310e51c4899b
RobberBadeia <robber.badeia@gmail.com>
2024-01-25 15:58:54 +0200

    .

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X POST -H "email: test@gmail.com" -H "subject:I will always be here for PLD"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
